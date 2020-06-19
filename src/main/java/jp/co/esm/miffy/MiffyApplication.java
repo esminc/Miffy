8d3005a488dab9ba20eccbef93ee2c0403fa488b
uesaka-seiya <seiya.uesaka@esm.co.jp>
2020-06-19 14:57:05 +0900

    first commit

---
+++ src/main/java/jp/co/esm/miffy/MiffyApplication.java
@@ -0,0 +1,13 @@
+package jp.co.esm.miffy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiffyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiffyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/co/esm/miffy/MiffyApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.esm.miffy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiffyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
