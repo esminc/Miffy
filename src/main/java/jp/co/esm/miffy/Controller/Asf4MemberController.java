5b2b280644f10a192f4474240430b54225c27067
uesaka-seiya <seiya.uesaka@esm.co.jp>
2020-06-25 10:33:35 +0900

    chore: `@RequestMapping` の処理を `@GetMapping` に変更した

--- src/main/java/jp/co/esm/miffy/Controller/Asf4MemberController.java
+++ src/main/java/jp/co/esm/miffy/Controller/Asf4MemberController.java
@@ -5,8 +5,7 @@ import jp.co.esm.miffy.repository.Asf4MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
@@ -16,7 +15,7 @@ public class Asf4MemberController {
   @Autowired
   Asf4MemberRepository asf4MemberRepository;
 
-  @RequestMapping(value = "asf4members", method = RequestMethod.GET)
+  @GetMapping("asf4members")
   public String index(Model model) {
     List<Asf4Member> asf4MemberList=asf4MemberRepository.findAll();
     model.addAttribute("asf4MemberList", asf4MemberList);
