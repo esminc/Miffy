87816d2b54cd1a48fd962bf3a80823fa481a7295
uesaka-seiya <seiya.uesaka@esm.co.jp>
2020-06-25 13:01:13 +0900

    add: Serviceクラスを作成した

--- src/main/java/jp/co/esm/miffy/Controller/Asf4MemberController.java
+++ src/main/java/jp/co/esm/miffy/Controller/Asf4MemberController.java
@@ -1,7 +1,7 @@
 package jp.co.esm.miffy.Controller;
 
 import jp.co.esm.miffy.entity.Asf4Member;
-import jp.co.esm.miffy.repository.Asf4MemberRepository;
+import jp.co.esm.miffy.service.Asf4MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,11 +13,11 @@ import java.util.List;
 public class Asf4MemberController {
 
   @Autowired
-  Asf4MemberRepository asf4MemberRepository;
+  Asf4MemberService asf4MemberService;
 
   @GetMapping("asf4members")
   public String index(Model model) {
-    List<Asf4Member> asf4MemberList=asf4MemberRepository.findAll();
+    List<Asf4Member> asf4MemberList = asf4MemberService.selectAll();
     model.addAttribute("asf4MemberList", asf4MemberList);
     return "asf4members";
   }
---
+++ src/main/java/jp/co/esm/miffy/service/Asf4MemberService.java
@@ -0,0 +1,20 @@
+package jp.co.esm.miffy.service;
+
+import jp.co.esm.miffy.entity.Asf4Member;
+import jp.co.esm.miffy.repository.Asf4MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class Asf4MemberService {
+
+  @Autowired
+  private Asf4MemberRepository asf4MemberRepository;
+
+  public List<Asf4Member> selectAll() {
+    List<Asf4Member> asf4MemberList = asf4MemberRepository.findAll();
+    return asf4MemberList;
+  }
+}
