7f7499eb52b233b9cebd34b08245986512500f62
RyuyaMaeda <ryuya.maeda@esm.co.jp>
2020-06-29 09:18:18 +0900

    add: レスポンスをバインドするフィールドをもつTestResponseResourceクラスを作成した

---
+++ src/main/java/jp/co/esm/miffy/service/TestResponseResource.java
@@ -0,0 +1,11 @@
+package jp.co.esm.miffy.service;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TestResponseResource {
+    private String id;
+    private String message;
+}
