e9c919a90a2b9dfb19a15d878a3730ffb8dd5a20
RyuyaMaeda <ryuya.maeda@esm.co.jp>
2020-06-29 09:47:34 +0900

    chore: 指定URLにpostリクエストするgetTestResponse()をserviceクラスに作成した

--- src/main/java/jp/co/esm/miffy/service/Asf4MemberService.java
+++ src/main/java/jp/co/esm/miffy/service/Asf4MemberService.java
@@ -4,17 +4,42 @@ import jp.co.esm.miffy.entity.Asf4Member;
 import jp.co.esm.miffy.repository.Asf4MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class Asf4MemberService {
-  public final Asf4MemberRepository asf4MemberRepository;
+    public final Asf4MemberRepository asf4MemberRepository;
+    private RestTemplate restTemplate;
 
-  public List<Asf4Member> selectAll() {
-    List<Asf4Member> asf4MemberList = asf4MemberRepository.findAll();
-    return asf4MemberList;
-  }
+    @Autowired
+    public Asf4MemberService(Asf4MemberRepository asf4MemberRepository, RestTemplateBuilder builder) {
+        this.asf4MemberRepository = asf4MemberRepository;
+        this.restTemplate = builder.build();
+    }
+
+    public static final String URL = "https://idobata.io/hook/custom/40767f01-0b3d-4065-8770-d9e25a206c24";
+
+    public List<Asf4Member> selectAll() {
+        List<Asf4Member> asf4MemberList = asf4MemberRepository.findAll();
+        return asf4MemberList;
+    }
+
+    public TestResponseResource getTestResponse() {
+        TestRequestResource request = new TestRequestResource();
+        request.setMessage("test message");
+        try {
+            return restTemplate.postForObject(URL, request, TestResponseResource.class);
+        } catch (HttpClientErrorException e) {
+            throw e;
+        } catch (HttpServerErrorException e) {
+            throw e;
+        }
+    }
 }
