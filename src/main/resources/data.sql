afc809a5326b2c822cffe365a8fd6ce2c110c843
uesaka-seiya <seiya.uesaka@esm.co.jp>
2020-07-01 18:01:42 +0900

    chore: data.sqlに登録したidobataIDを修正した

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO members (name, idobata_id) VALUES
-('永和 一郎', 'here'),('永和 二郎', 'here2');
+('永和 一郎', 'here1'),('永和 二郎', 'here2'),('前田 竜也', 'RyuyaMaeda'), ('上坂 静耶', 'uesaka-seiya');
 
 INSERT INTO members (name, idobata_id, skip) VALUES
-('永和 二郎', 'all', TRUE),('永和 三郎', 'test', TRUE);
+('永和 二郎', 'all1', TRUE),('永和 三郎', 'test', TRUE);
