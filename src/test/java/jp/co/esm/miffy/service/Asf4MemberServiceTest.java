3a47431783d1bb461b3ab46ec3c2af804b1cbbd3
uesaka-seiya <seiya.uesaka@esm.co.jp>
2020-07-09 16:15:52 +0900

    add: selectByIdobataId()のテストを作った

--- src/test/java/jp/co/esm/miffy/service/Asf4MemberServiceTest.java
+++ src/test/java/jp/co/esm/miffy/service/Asf4MemberServiceTest.java
@@ -17,6 +17,7 @@ class Asf4MemberServiceTest {
     @Autowired
     private Asf4MemberService asf4MemberService;
 
+    @Test
     @Sql(statements = {"DROP TABLE members;",
             "CREATE TABLE if not exists members " +
                     "(id SERIAL NOT NULL PRIMARY KEY, name VARCHAR(255) NOT NULL, idobata_id VARCHAR(20), floor VARCHAR(20) DEFAULT 4, skip BOOLEAN DEFAULT FALSE);",
@@ -24,8 +25,6 @@ class Asf4MemberServiceTest {
                     "('スナッフィー', 'test1', FALSE), " +
                     "('ボリス', 'test2', FALSE)," +
                     "('バーバラ', 'test3', TRUE);"})
-
-    @Test
     public void selectAllTest() {
         List<Asf4Member> expectedMembers = new ArrayList<>();
         expectedMembers.add(new Asf4Member(1,"スナッフィー", "test1", "4", false));
@@ -33,6 +32,20 @@ class Asf4MemberServiceTest {
         expectedMembers.add(new Asf4Member(3,"バーバラ", "test3", "4", true));
         List<Asf4Member> actualMembers = asf4MemberService.selectAll();
         assertEquals(expectedMembers, actualMembers);
+    }
+
+    @Test
+    @Sql(statements = {"DROP TABLE members;",
+            "CREATE TABLE if not exists members " +
+                    "(id SERIAL NOT NULL PRIMARY KEY, name VARCHAR(255) NOT NULL, idobata_id VARCHAR(20), floor VARCHAR(20) DEFAULT 4, skip BOOLEAN DEFAULT FALSE);",
+            "INSERT INTO members (name, idobata_id, skip) VALUES " +
+                    "('スナッフィー', 'test1', FALSE), " +
+                    "('ボリス', 'test2', FALSE)," +
+                    "('バーバラ', 'test3', TRUE);"})
+    public void selectByIdobataIdTest() {
+        Asf4Member expectedSelectedMember = new Asf4Member(1,"スナッフィー", "test1", "4", false);
+        Asf4Member actualSelectedMember = asf4MemberService.selectByidobataId("test1");
+        assertEquals(expectedSelectedMember, actualSelectedMember);
     }
 
 }
