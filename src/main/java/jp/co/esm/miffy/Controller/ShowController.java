407fcd949542a6b184b6c6a5c77b17bf8431b119
RyuyaMaeda <ryuya.maeda@esm.co.jp>
2020-07-10 13:02:31 +0900

    fix: 二重送信の対策のため、history.back()を使わずに、URLで画面を指定するようにした

--- src/main/java/jp/co/esm/miffy/Controller/ShowController.java
+++ src/main/java/jp/co/esm/miffy/Controller/ShowController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
@@ -14,7 +15,7 @@ import java.util.List;
 public class ShowController {
     private final Asf4MemberService asf4MemberService;
 
-    @GetMapping("asf4members")
+    @RequestMapping("asf4members")
     public String index(Model model) {
         List<Asf4Member> asf4MemberList = asf4MemberService.selectAll();
         model.addAttribute("asf4MemberList", asf4MemberList);
